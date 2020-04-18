07cf364ad62802246001af841f2984a953f266e3
Blake <blake@futchi.com>
2020-04-18 18:56:45 +0200

    vh change hero image

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -6,7 +6,7 @@ const square = {width: 225, height: 225}
 
 const Hero = () => {
   return (
-    <LazyHero imageSrc="https://cdn.hipwallpaper.com/i/35/78/jhYV6A.jpg" style={{ marginTop: '-15px', opacity: 1, minHeight: '90vh'}}>
+    <LazyHero imageSrc="https://cdn.hipwallpaper.com/i/35/78/jhYV6A.jpg" style={{ marginTop: '-15px', opacity: 1, minHeight: '85vh'}}>
       <Segment circular style ={square}>
         <Header disabled>GET YOUR BASIC NEEDS AND SHOPPING TAKEN CARE OF FROM THE COMFORT OF YOUR HOME</Header>
       </Segment>
